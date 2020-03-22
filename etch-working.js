d9e2a8476d8517c59444965ecbce3f4b198c999f
Carl D'Oleo-Lundgren <carl.doleo.lundgren@gmail.com>
2020-03-22 10:54:44 -0500

    Added working redraw grid button

--- etch-working.js
+++ etch-working.js
@@ -1,17 +1,17 @@
-var n=16; //take grid column value as you want
-
 const bigContainer = document.querySelector('.bigContainer')
 
-for(let i = 1; i < n; i++) {
-    bigContainer.innerHTML+='<div class="row">';
-
-    for(let j = 0; j < n; j++) {
-        bigContainer.innerHTML+='<div class="smallBox">';
+window.onload = function initializeGrid() {
+    for(let i = 0; i < 16; i++) {
+        bigContainer.innerHTML+='<div class="row">';
+    
+        for(let j = 0; j < 16; j++) {
+            bigContainer.innerHTML+='<div class="smallBox">';
+        }
     }
 }
 
-bigContainer.addEventListener('mouseover', e => {
-    let target = e.target
+bigContainer.addEventListener('mouseover', event => {
+    let target = event.target
   
     if (target !== bigContainer) {
       target.classList.add('permahover')
@@ -21,6 +21,21 @@ bigContainer.addEventListener('mouseover', e => {
 const resetButton = document.querySelector('#resetButton');
 resetButton.addEventListener('click', () => {
     const smallBoxes = document.querySelectorAll('.smallBox');
-
     smallBoxes.forEach(box => box.classList.remove('permahover'));
 });
+
+const redrawGridButton = document.querySelector('#redrawGridButton');
+redrawGridButton.addEventListener('click', () => {
+    bigContainer.innerHTML = "";
+
+    let numBoxes = prompt("How many boxes do you want per row?");
+
+    for(let i = 0; i < numBoxes; i++) {
+        bigContainer.innerHTML+='<div class="row">';
+    
+        for(let j = 0; j < numBoxes; j++) {
+            bigContainer.innerHTML+='<div class="smallBox">';
+        }
+    }
+});
+
